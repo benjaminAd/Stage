543aa8727b77a6733eec4c3175b536e74f1ccbb6
Matthias Berjola <matthias.berjola@gmail.com>
2019-12-18 21:19:58 +0400

    page mention legal mis en forme

--- resources/views/Maquette mentionLegale/mentionLegal.blade.php
+++ resources/views/Maquette mentionLegale/mentionLegal.blade.php
@@ -3,18 +3,24 @@
     <title>Mentions légales du site CONNECTANOU</title>
 @endsection
 @section('link')
-     
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 @endsection
 
 @section('body')
+<div class="py-5" style="justify-content:center;">
 @include('navbar')
-    <h1>Mentions légales et politiques de confidentialité de Connectanou</h1>
+<br><br><br><br><br><br>
 
-<div class="mention2">
+<h1 style="text-align:center">Mentions légales et politiques de confidentialité de Connectanou</h1>
+
+
     <br>
-    <h2>Présentation du site</h2>
-</div>
-<p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est
+    <h2 style="text-align:center">Présentation du site</h2>
+
+<p class="font-weight-normal">En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est
     précisé aux utilisateurs du site www.connectanou.re l'identité des différents intervenants dans le cadre de sa
     réalisation et de son suivi : <br/><br/>
     Le présent site est édité par : Jenna POURCHOT, Présidente de <strong>l'Association Connectanou</strong> ayant
@@ -25,11 +31,11 @@
     Webmaster du site: <strong>CONNECTANOU</strong>, joingnable par email sur connectanou974@gmail.com .<br/><br/>
     Hébergeur du site: OVH Group SAS, qui est domicilé au 2 rue Kellerman 59100 Roubaix, déclaré au RCS Lille
     Métropole 424 761 419 00045. </p>
-
-<div class="mention2">
-    <h2>Propriété intellectuelle et contrefaçons</h2>
 </div>
-<p>L’Association CONNECTANOU est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Propriété intellectuelle et contrefaçons</h2>
+
+<p class="font-weight-normal">L’Association CONNECTANOU est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage
     sur tous les éléments accessibles sur le site, notamment : les textes, les images, les graphismes, le logo, les
     icônes,… <br/><br/>
     Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du
@@ -38,11 +44,11 @@
     Toute exploitation non autorisée du site ou d’un quelconque élément qu’il contient sera considérée comme
     constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du
     Code de Propriété Intellectuelle.<br/>
-
-<div class="mention2">
 </div>
-<h2>Cookies et traceurs</h2>
-<p>Le site internet <a href="https://www.connectanou.re">www.connectanou.re</a> possède un système de mesure
+<div class="py-5" style="justify-content:center">
+
+<h2 style="text-align:center">Cookies et traceurs</h2>
+<p class="font-weight-normal">Le site internet <a href="https://www.connectanou.re">www.connectanou.re</a> possède un système de mesure
     d’audience, ainsi qu’une fonction de partage sur les <strong>réseaux sociaux</strong>.<br/><br/>
     En application de la directive européenne dite « paquet télécom », les internautes doivent être informés et
     donner leur consentement préalablement à l’insertion de traceurs (plus couramment appelés « cookies »).<br/>
@@ -51,32 +57,32 @@
     préalable le consentement des utilisateurs.<br/><br/>
     Le refus d’installation d’un cookie peut entraîner l’impossibilité d’accéder à certains services. <br/>L’internaute
     peut toutefois configurer son navigateur internet pour refuser l’installation des cookies.
-
-<div class="mention2">
-    <h2>Mesure d’audience</h2>
 </div>
-<p>Les cookies de mesure d’audience sont de petits fichiers qui permettent de connaitre et d’analyser les
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Mesure d’audience</h2>
+
+<p class="font-weight-normal">Les cookies de mesure d’audience sont de petits fichiers qui permettent de connaitre et d’analyser les
     statistiques de trafic sur le site internet : les pages visitées, le nombre de visites, le taux de rebond, la
     provenance des visites,… <br/><br/>Les cookies de mesure d’audience sont totalement anonymes.<br/>
     Sur ce site, c’est la solution Google Analytics qui est utilisée pour mesurer l’audience.
-
-<div class="mention2">
-    <h2>Réseaux sociaux</h2>
 </div>
-<p>Les cookies liés aux réseaux sociaux sont associés aux boutons qui facilitent le partage des pages et articles
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Réseaux sociaux</h2>
+
+<p class="font-weight-normal">Les cookies liés aux réseaux sociaux sont associés aux boutons qui facilitent le partage des pages et articles
     sur les réseaux sociaux.
-
-<div class="mention2">
-    <h2>Formulaire de contact et commentaires</h2>
 </div>
-<p>Vous pouvez être amené à nous indiquer votre adresse e-mail lorsque vous remplissez notre formulaire de contact
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Formulaire de contact et commentaires</h2>
+
+<p class="font-weight-normal">Vous pouvez être amené à nous indiquer votre adresse e-mail lorsque vous remplissez notre formulaire de contact
     ou déposez un commentaire sur l’un des articles du site site www.connectanou.re.<br/><br/>
     En aucun cas, votre adresse e-mail ne sera cédée à des tiers.
-
-<div class="mention2">
-    <h2>Modération des commentaires</h2>
 </div>
-<p>Le choix de validation d’un commentaire sur le site <a href="https://www.connectanou.re">www.connectanou.re</a>
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Modération des commentaires</h2>
+
+<p class="font-weight-normal">Le choix de validation d’un commentaire sur le site <a href="https://www.connectanou.re">www.connectanou.re</a>
     est laissé à l’entière appréciation du responsable de publication. Les commentaires peuvent être supprimés,
     modifiés et corrigés pour une meilleure compréhension des visiteurs (notamment pour l’orthographe).<br/><br/>
     L’internaute peut signer son commentaire de son nom ou pseudo ou nom de son entreprise. Il peut également
@@ -85,41 +91,39 @@
     publié.
     Voici des exemples de cas ou un commentaire peut-être modéré ou supprimé :
 
-<ul>
-    <li>Il a été supprimé par l’anti-spam.</li>
-    <li>Il n’apporte pas réellement de plus value et n’est pas utile pour les internautes.</li>
-    <li>Il est truffé de fautes d’orthographe ou incompréhensible.</li>
-    <li>Il semble être déposé uniquement dans un but auto-promotionnel.</li>
-    <li>Le mail indiqué est visiblement faux.</li>
-    <li>Il est jugé diffamatoire pour un tiers.</li>
-</ul>
 
-<div class="mention2">
-    <h2>Newsletter</h2>
+<ul class="list-group-flush">
+<li class="list-group-item">Il a été supprimé par l’anti-spam.</li>
+    <li class="list-group-item">Il n’apporte pas réellement de plus value et n’est pas utile pour les internautes.</li>
+    <li class="list-group-item">Il est truffé de fautes d’orthographe ou incompréhensible.</li>
+    <li class="list-group-item">Il semble être déposé uniquement dans un but auto-promotionnel.</li>
+    <li class="list-group-item">Le mail indiqué est visiblement faux.</li>
+    <li class="list-group-item">Il est jugé diffamatoire pour un tiers.</li>
+</ul>
 </div>
-<p>Vous pouvez vous abonner à la newsletter du site. Vous recevez alors automatiquement et gratuitement des
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Newsletter</h2>
+
+<p class="font-weight-normal">Vous pouvez vous abonner à la newsletter du site. Vous recevez alors automatiquement et gratuitement des
     newsletters traitants les sujets du site <a href="https://www.connectanou.re">www.connectanou.re</a>.<br/><br/>
     Vous pouvez vous désinscrire à tout moment de la newsletter en cliquant sur le lien de désabonnement présent en
     bas de chaque newsletter.<br/>
     En aucun cas, votre adresse e-mail ne sera cédée à des tiers.
-
-<div class="mention2">
-    <h2>Liens hypertextes</h2>
 </div>
-<p>Ce site internet contient un certain nombre de liens hypertextes vers d’autres sites.<br/> Cependant, <a
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Liens hypertextes</h2>
+
+<p class="font-weight-normal">Ce site internet contient un certain nombre de liens hypertextes vers d’autres sites.<br/> Cependant, <a
             href="https://www.connectanou.re">www.connectanou.re</a> n’a pas la possibilité de suivre et vérifier le
     contenu de ces sites, et n’assumera en conséquence aucune responsabilité de ce fait.
-
-<div class="mention2">
-    <h2>Flux RSS</h2>
 </div>
-<p>Les flux RSS sont exclusivement destinés aux visiteurs du site pour une utilisation personnelle et ne sauraient
+<div class="py-5" style="justify-content:center">
+    <h2 style="text-align:center">Flux RSS</h2>
+
+<p class="font-weight-normal">Les flux RSS sont exclusivement destinés aux visiteurs du site pour une utilisation personnelle et ne sauraient
     en aucun cas servir à alimenter d’autres sites, sauf autorisation écrite préalable de <a
             href="https://www.connectanou.re">www.connectanou.re</a>.
-<p> &nbsp;</p>
-<p> &nbsp;</p>
-<p> &nbsp;</p>
-<p> &nbsp;</p>
+</div>
 @include('footer')
 @endsection
 @section('script')
